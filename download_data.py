44cb8266a8591c5dd7df606506c23a1139470339
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 03:09:32 +0000

    Adding tarfile member sanitization to extractall()

--- download_data.py
+++ download_data.py
@@ -65,7 +65,26 @@ def download_salsa(resource_manager):
         logging.info('Extracting [%s] from [%s] to [%s]', source_file, archive, target_file)
         with tarfile.open(archive) as tar:
             tmp_dir = tempfile.mkdtemp()
-            tar.extractall(tmp_dir)
+            def is_within_directory(directory, target):
+            	
+            	abs_directory = os.path.abspath(directory)
+            	abs_target = os.path.abspath(target)
+            
+            	prefix = os.path.commonprefix([abs_directory, abs_target])
+            	
+            	return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+            	for member in tar.getmembers():
+            		member_path = os.path.join(path, member.name)
+            		if not is_within_directory(path, member_path):
+            			raise Exception("Attempted Path Traversal in Tar File")
+            
+            	tar.extractall(path, members, numeric_owner=numeric_owner) 
+            	
+            
+            safe_extract(tar, tmp_dir)
             shutil.move(os.path.join(tmp_dir, source_file), target_file)             
 
     logging.info('Downloading SALSA data')
